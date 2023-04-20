35c2c4068a00df8b8c382f88e43714972ae9857b
Jordan Bussanich <jordan@bussanich.ca>
2023-04-20 08:14:10 -0400

    Added License notice

--- sqlite_search.py
+++ sqlite_search.py
@@ -1,10 +1,27 @@
+# SQLite Search
+# Hacked together by Jordan Bussanich
+
+# Copyright (C) 2023  Jordan Bussanich
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 import sqlite3
 import argparse
 
+# tabulate is an MIT licensed library and can be found here: https://pypi.org/project/tabulate/
 from tabulate import tabulate
-
-# SQLite Search
-# Hacked together by Jordan Bussanich
 
 def search_sqlite(input_file: str, search_text: str, case_sensitive: bool = False, show_details: bool = False) -> None:
     print(f"Opening database '{input_file}' in read-only mode.")
