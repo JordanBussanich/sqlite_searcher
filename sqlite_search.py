79ed8618a77427a1d0cfe0deb4152516e732fe3b
Jordan Bussanich <jordan@bussanich.ca>
2026-06-23 08:54:44 -0400

    Code cleanup, trying to stick to 80 chars wide. Force GPLv2 only.

--- sqlite_search.py
+++ sqlite_search.py
@@ -1,21 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2023, 2026  Jordan Bussanich
+
 # SQLite Search
 # Hacked together by Jordan Bussanich
-
-# Copyright (C) 2023  Jordan Bussanich
-
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 import sqlite3
 import argparse
@@ -25,12 +12,18 @@ import sys
 
 from abc import ABC, abstractmethod
 
-# tabulate is an MIT licensed library and can be found here: https://pypi.org/project/tabulate/
+# tabulate is an MIT licensed library and can be found here: 
+# https://pypi.org/project/tabulate/
 from tabulate import tabulate
 
 
 class RowSearchResult:
-    def __init__(self, search_term: str, result: str, column_names: list[str], table_name: str, row: list) -> None:
+    def __init__(self, 
+                 search_term: str, 
+                 result: str, 
+                 column_names: list[str],
+                 table_name: str, 
+                 row: list) -> None:
         self.search_term = search_term
         self.result = result
         self.column_names = column_names
@@ -38,12 +31,22 @@ class RowSearchResult:
         self.row = row
     
     def __hash__(self) -> int:
-        return hash((self.search_term, self.result, ','.join(self.column_names), ','.join(map(str, self.row)), self.table_name))
+        return hash((self.search_term, 
+                     self.result, 
+                     ','.join(self.column_names), 
+                     ','.join(map(str, self.row)),
+                     self.table_name))
     
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, type(self)):
             return NotImplemented
-        return self.search_term == __value.search_term and self.result == __value.result and self.column_names == __value.column_names and self.table_name == __value.table_name and self.row == __value.row
+        return (
+            self.search_term == __value.search_term and
+            self.result == __value.result and
+            self.column_names == __value.column_names and
+            self.table_name == __value.table_name and
+            self.row == __value.row
+        )
 
 
 class TableSearchResult:
@@ -57,7 +60,10 @@ class TableSearchResult:
     def __eq__(self, __value: object) -> bool:
         if not isinstance(__value, type(self)):
             return NotImplemented
-        return self.search_term == __value.search_term and self.result == __value.result
+        return (
+            self.search_term == __value.search_term and
+            self.result == __value.result
+        )
 
 
 class CellSearcher(ABC):
@@ -98,7 +104,10 @@ class RegexCellSearcher(CellSearcher):
             self.regex = re.compile(search_term, re.IGNORECASE)       
 
 
-def search_sqlite(searchers: list[CellSearcher], input_file: str) -> typing.Tuple[list[TableSearchResult], list[RowSearchResult]]:
+def search_sqlite(
+        searchers: list[CellSearcher], 
+        input_file: str
+) -> tuple[list[TableSearchResult], list[RowSearchResult]]:
     table_search_results = list[TableSearchResult]()
     row_search_results = list[RowSearchResult]()
     
@@ -106,9 +115,10 @@ def search_sqlite(searchers: list[CellSearcher], input_file: str) -> typing.Tupl
 
     conn = sqlite3.connect(rf"{input_file}")
 
+    # Exclude sqlite system tables
     get_tables_query = """SELECT name 
                           FROM sqlite_master 
-                          WHERE type='table' AND name NOT LIKE 'sqlite%'"""     # Exclude sqlite system tables
+                          WHERE type='table' AND name NOT LIKE 'sqlite%'"""
 
     cursor = conn.cursor()
 
@@ -127,7 +137,9 @@ def search_sqlite(searchers: list[CellSearcher], input_file: str) -> typing.Tupl
     for table in tables:
         for searcher in searchers:
             if searcher.search_cell(table):
-                table_name_matches.add(TableSearchResult(searcher.search_term, table))
+                table_name_matches.add(
+                    TableSearchResult(searcher.search_term, table)
+                )
     
 
     if len(table_name_matches) > 0:
@@ -147,7 +159,13 @@ def search_sqlite(searchers: list[CellSearcher], input_file: str) -> typing.Tupl
             for col in row:
                 for searcher in searchers:
                     if searcher.search_cell(col):
-                        row_matches.add(RowSearchResult(searcher.search_term, col, column_names, table, row))
+                        row_matches.add(RowSearchResult(
+                            searcher.search_term, 
+                            col, 
+                            column_names, 
+                            table, 
+                            row
+                        ))
         
         if len(row_matches) > 0:
             row_search_results.extend(row_matches)
@@ -161,38 +179,55 @@ def search_sqlite(searchers: list[CellSearcher], input_file: str) -> typing.Tupl
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='sqlite_searcher',
-                                    description='This program searches SQLite files for a string')
+    parser = argparse.ArgumentParser(
+        prog='sqlite_searcher',
+        description='This program searches SQLite files for a string'
+    )
 
-    parser.add_argument('-i', '--input',
-                        help='The input SQLite database.',
-                        required=True,
-                        dest='input_file')
+    parser.add_argument(
+        '-i', 
+        '--input',
+        help='The input SQLite database.',
+        required=True,
+        dest='input_file'
+    )
 
-    parser.add_argument('-s', '--search-for',
-                        help='The text you want to search for.',
-                        dest='search_string')
+    parser.add_argument(
+        '-s',
+        '--search-for',
+        help='The text you want to search for.',
+        dest='search_string'
+    )
 
-    parser.add_argument('-k', '--keyword-list',
-                        help='A text file containing a single search term on each line. Do not use -s with -k.',
-                        dest='keyword_list')
+    parser.add_argument(
+        '-k',
+        '--keyword-list',
+        help='A text file containing a single search term on each line. Do not use -s with -k.',
+        dest='keyword_list'
+    )
 
-    parser.add_argument( '--regex',
-                        action='store_true',
-                        help='Search using Regexs.')
+    parser.add_argument(
+        '--regex',
+        action='store_true',
+        help='Search using Regexs.'
+    )
 
     # This is disabled until it's implemented. A cool way to do that would be to build an "Outputter" class
     # parser.add_argument('--csv',
     #                     action='store_true',
     #                     help='Output a CSV of each result, with the Table name in the leftmost column. This script does not write the CSV to the disk, only to STDOUT')
 
-    parser.add_argument('--case-sensitive',
-                        action='store_true',
-                        help='Search for a case-sensitive string. Do not use this with --regex.')
+    parser.add_argument(
+        '--case-sensitive',
+        action='store_true',
+        help='Search for a case-sensitive string. Do not use this with --regex.'
+    )
 
-    parser.add_argument('--show-details',
-                        action='store_true',
-                        help='Show each row that contains the search text, rather than just the counts.')
+    parser.add_argument(
+        '--show-details',
+        action='store_true',
+        help='Show each row that contains the search text, rather than just the counts.'
+    )
 
     arguments = parser.parse_args()
 
@@ -215,9 +250,13 @@ if __name__ == "__main__":
     searchers = list[CellSearcher]()
     for keyword in keywords:
         if arguments.regex:
-            searchers.append(RegexCellSearcher(keyword, arguments.case_sensitive))
+            searchers.append(
+                RegexCellSearcher(keyword, arguments.case_sensitive)
+            )
         else:
-            searchers.append(TextCellSearcher(keyword, arguments.case_sensitive))
+            searchers.append(
+                TextCellSearcher(keyword, arguments.case_sensitive)
+            )
 
     results = search_sqlite(searchers, arguments.input_file)
 
@@ -226,8 +265,17 @@ if __name__ == "__main__":
     # Table names
     if len(results[0]) > 0:
         print("Found the following keywords in the following Table names:")
-        table_output = zip([r.search_term for r in results[0]], [r.result for r in results[0]])
-        print(tabulate(table_output, ('Keyword', 'Table'), tablefmt='mixed_outline'))
+        table_output = zip(
+            [r.search_term for r in results[0]], 
+            [r.result for r in results[0]]
+        )
+        print(
+            tabulate(
+                table_output,
+                ('Keyword', 'Table'),
+                tablefmt='mixed_outline'
+            )
+        )
     else:
         print('No keywords found in any Table names.')
 
@@ -252,7 +300,13 @@ if __name__ == "__main__":
                         first = False
                 
                 print(f"Keyword matches for '{table}':")
-                print(tabulate(table_output_rows, column_headers, tablefmt='mixed_outline'))
+                print(
+                    tabulate(
+                        table_output_rows,
+                        column_headers,
+                        tablefmt='mixed_outline'
+                    )
+                )
                 print()
         else:
             result = dict()
@@ -266,7 +320,9 @@ if __name__ == "__main__":
                 
             for table, matches in result.items():
                 for keyword, count in matches.items():
-                    print(f"Found {count} instances of '{keyword}' in '{table}'.")
+                    print(
+                        f"Found {count} instances of '{keyword}' in '{table}'."
+                    )
             
     else:
         print('No keywords found in any Tables.')
