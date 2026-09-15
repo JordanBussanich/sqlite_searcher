904ebc590a072cde9392e5cfef6bf437996a8bd1
Jordan Bussanich <jordan@bussanich.ca>
2026-09-15 10:51:07 -0400

    Changed the build system to Nuitka/MSVC as Windows Defender was flagging the resultant EXE file as malware.

--- version.py
+++ version.py
@@ -9,11 +9,7 @@ import tomllib
 
 from pathlib import Path
 
-base_path: str
-if getattr(sys, "frozen", False):
-    base_path = Path(sys._MEIPASS)
-else:
-    base_path = Path(__file__).resolve().parent
+base_path = Path(__file__).resolve().parent
 
 with open(base_path / "pyproject.toml", "rb") as version_file:
     config = tomllib.load(version_file)
