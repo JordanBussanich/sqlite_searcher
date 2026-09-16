d0b973c0be1b072ba3b8f5240e8f8bda5163cea2
Jordan Bussanich <jordan@bussanich.ca>
2026-09-16 08:08:08 -0400

    Moved the UI init logic to the ui module.

---
+++ ui/ui.py
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C)  2026  Jordan Bussanich
+
+# SQLite Search UI
+# Hacked together by Jordan Bussanich
+
+import i18n
+import tkinter as tk
+
+from pathlib import Path
+
+from .main.controller import MainController
+from .main.view import MainView
+from version import __version__
+
+def start() -> None:
+    root = tk.Tk()
+
+    translations_folder = Path(__file__).parent.resolve() / "translations"
+
+    # Uncomment to test it in French
+    #i18n.set("locale", "fr")
+
+    i18n.load_path.append(str(translations_folder))
+
+    # Ignores the locale in the root of the JSON files
+    i18n.set("skip_locale_root_data", True)
+
+    # namespace maps to the View name so ui/translations/en/<namespace>.json
+    i18n.set("filename_format", "{namespace}.{format}")
+
+    # Read the locale from the folder names in ui/translations
+    i18n.set("use_locale_dirs", True)
+
+    print(i18n.t("sqlite_file"))
+
+    controller = MainController()
+
+    view = MainView(root, controller, __version__)
+
+    controller.bind_view(view)
+
+    root.mainloop()
